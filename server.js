29804e3b6d95caf9eb408329a8fc0b97f27dce76
Charles-André Flamand <caflamand@users.noreply.github.com>
2019-06-15 18:29:46 -0400

    Add db (#1)
    * Add endb + update mechanisms
    
    * Update gitignore
    
    * Get nodes endpoint

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const request = require('request');
+const Datastore = require('nedb');
+
+let db = new Datastore({filename: 'nodes.db', autoload: true});
 
 const port = 3000;
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/test", function(req, res) {
-  console.log(req);
+app.get("/test", function (req, res) {
+    console.log(req);
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
+
+app.get("/api/v1/nodes", function (req, res) {
+    db.find({}, function (err, docs) {
+        res.send(docs);
+    })
+});
+
+function queryProm() {
+    request('http://172.16.45.19:9090/api/v1/query?query=up', {json: true}, (err, res, body) => {
+            let data = body.data.result;
+
+            for (let i = 0; i < data.length; i++) {
+                let instance = data[i].metric.instance;
+                let status = data[i].value[1];
+
+                db.findOne({name: instance}, (err, result) => {
+                    if (result === null) {
+                        console.log("Inserting: " + instance);
+                        db.insert({
+                            name: instance,
+                            status: status,
+                            pos: {
+                                x: 0,
+                                y: 0
+                            }
+                        })
+                    }
+                });
+                db.update({name: instance}, {$set: {status: status}});
+            }
+            db.loadDatabase();
+        }
+    );
+}
+
+setInterval(queryProm, 1000);
 
 app.listen(3000);
